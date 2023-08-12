fedd168e13f3b81c35ab98e3e96c9dbb523559cf
tufankilicc <129671365+tufankilicc@users.noreply.github.com>
2023-08-12 22:44:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ def main():
     st.title("WELCOME TO")
     
     if st.button("BUTONA TIKLA"):
-        st.write("AMCA BENİ AFFET".upper())
+        st.header("AMCA BENİ AFFET".upper())
         st.balloons()
 
 if __name__ == "__main__":
