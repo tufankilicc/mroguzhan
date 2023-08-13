0b64ea05ad75557bc588a7e7a44974b2647897aa
tufankilicc <129671365+tufankilicc@users.noreply.github.com>
2023-08-13 11:54:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ def main():
         st.balloons()
         st.success("AMCAAAAAAAAAAAA BOĞULUYORUMMMMMMMM")
         st.error("AMCAAA BANA BAKLAVA VEEEEER")
+        imgurl='https://www.baktad.org.tr/wp-content/uploads/2021/02/cengizbayindir.jpg'
+        st.image(imgurl, caption='Örnek Resim', use_column_width=True)
 
 if __name__ == "__main__":
     main()
