868521d53b5295783e628f3f0fdc0ce73b394047
tufankilicc <129671365+tufankilicc@users.noreply.github.com>
2023-08-13 11:59:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ def main():
         imgurl='https://www.baktad.org.tr/wp-content/uploads/2021/02/cengizbayindir.jpg'
         st.image(imgurl, caption='Örnek Resim', use_column_width=True)
         imgurl2='https://media.licdn.com/dms/image/D4D03AQGGRw7tt3ncmQ/profile-displayphoto-shrink_800_800/0/1689504553913?e=2147483647&v=beta&t=EEW2IQkxhDk3OiICB1LtEEHfQyJz8Jl9VbmcnGzziLw'
-        st.image(imgurl2)
+        st.image(imgurl2,width=300)
 if __name__ == "__main__":
     main()
